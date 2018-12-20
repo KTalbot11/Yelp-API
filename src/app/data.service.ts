fbd94bdc8f151e6a4cd00002944d048302e8bb13
Keagan Talbot <kigogie22@gmail.com>
2018-12-20 15:20:07 -0500

    first commit

---
+++ src/app/data.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class DataService {
+ public YelpKey = "PQVN-lodhJx11sOJTd5xqRjfHGfje5pi-R7OfXEIgHBrFOcqYhkg6_vKBUV5RS8IuB8g3DkphwcaJ93XgOD04kcQM3slpIlDkD1U6_lj3EMlT6__Y06PrHNKPegbXHYx"
+ public APIurl = "https://api.yelp.com/v3"
+  constructor() { }
+}
